7061d7f9558f9198c8bdcfd921a579e7d7b47aa6
Eduard bc2025-2 <eduardcer2709@gmail.com>
2025-09-13 22:10:06 +0300

    Add main.js

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-// hello.js
-const githubUsername = "Eduard-2709";
-console.log(`Hello, ${githubUsername}`);
+const { URL } = require('url');
+
+// Константи для варіанту 1
+const currency = "USD";
+const date_from = "20220707";
+const date_to = "20220719";
+
+// Базовий URL
+const myUrl = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+
+// Додаємо параметри
+myUrl.searchParams.append("valcode", currency);
+myUrl.searchParams.append("start", date_from);
+myUrl.searchParams.append("end", date_to);
+myUrl.searchParams.append("json", "");
+
+// Виводимо у консоль
+console.log(myUrl.toString());
